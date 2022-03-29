2878baf6cc176183ffc5a7e8e2161057d35f865e
TheLichKingZeyd <45772157+TheLichKingZeyd@users.noreply.github.com>
2022-03-29 11:10:18 +0200

    Add files via upload

---
+++ test/GoTcpServer.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"testing"
+	"net"
+	"io"
+)
+func TestListener(t *testing.T) {
+
+	listener, err := net.Listen("tcp", "127.0.0.1:8080")
+    if err != nil {
+		t.Fatal(err)
+	}
+	
+	defer listener.Close()
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			t.Fatal(err)
+		}
+        // Anonym funksjon, kan brukes som parameter/variabel
+		go func() {
+            io.Copy(conn, conn)
+			conn.Close()
+		}()
+	}
+
+	t.Logf("Bundet til %q", listener.Addr())
+
+}
